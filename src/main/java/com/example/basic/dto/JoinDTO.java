63eb9052d7c560c7645c8162b6889b9386f0e4e2
ChangMin59 <dy2ghks@gmail.com>
2025-04-21 10:19:21 +0900

    lombok을 통해서 DTO구조 간소화

--- src/main/java/com/example/basic/dto/JoinDTO.java
+++ src/main/java/com/example/basic/dto/JoinDTO.java
@@ -1,34 +1,15 @@
 package com.example.basic.dto;
+import lombok.*;
 
-// 모든 폼 항목들을 멤버변수로 등록한뒤
-// 각 항목마다 getter, setter메서드 등록
-// 이런게 폼별로 DTO를 미리 준비해놓으면 폼 데이터를 파일단위로 구조적 관리 가능
-// 컨트롤러에서 파라미터 값 전달을 효율적으로 처리 가능
+//lombok라이브러리로부터 아래 어노테이션을 활성화 시키면
+// 매번 동일한 getter, setter, 생성자함수, 바환값 문자열 변환을 실행할 필요없음
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
 public class JoinDTO {
     private String uname;
     private String email;
     private String colors;
-
-    // 인스턴스 복사하는 생성자 함수
-    public JoinDTO(){}
-
-    // 각 폼의 항목별로 getter, setter등록
-    public String getUname(){
-        return uname;
-    }
-    public void setUname(String uname){
-        this.uname = uname;
-    }
-    public String getEmail(){
-        return email;
-    }
-    public void setEmail(String email){
-        this.email = email;
-    }
-    public String getColors(){
-        return colors;
-    }
-    public void setColors(String colors){
-        this.colors = colors;
-    }
 }
--- src/main/java/com/example/basic/service/JoinService.java
+++ src/main/java/com/example/basic/service/JoinService.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Service;
 public class JoinService {
     public String processJoin(JoinDTO dto) {
         // 컨트롤링를 통해 실제 템플릿에 전달된 데이터가 맵핑된 태그문자열 반환
+        // JoinDTO타입의 폼 객체를 전달받은 데이터를 활용한 태그 문자열 생성후 리턴
         String result = "name:" +dto.getUname()+"<br />"
                 + "email:" +dto.getEmail()+ "<br />"
                 + "my color:" +dto.getColors();
