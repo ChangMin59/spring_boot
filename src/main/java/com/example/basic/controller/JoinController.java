0c6c1ba565b41b9b30a293aeb07434dbf4ee4439
ChangMin59 <dy2ghks@gmail.com>
2025-04-22 11:10:54 +0900

    불필요한 코드 제거

--- src/main/java/com/example/basic/controller/JoinController.java
+++ src/main/java/com/example/basic/controller/JoinController.java
@@ -3,9 +3,7 @@ import com.example.basic.dto.JoinDTO;
 import com.example.basic.entity.JoinEntity;
 import com.example.basic.service.JoinService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-// Model 컨트롤러에서 템플릿에 데이터를 전달시 필요한 스프릿 내장
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
@@ -14,53 +12,30 @@ import org.springframework.web.bind.annotation.PostMapping;
 
 import java.util.List;
 
-// (URL)을 받아서, HTML 화면(View 페이지) 를 반환
 @Controller
-// 인스턴스를 담을 멤버변수에 final 지정 가능케함
+//인스턴스를 담을 멤버변수에 final 지정 가능케 함
 @RequiredArgsConstructor
 public class JoinController {
     private final JoinService joinService;
 
-    // join 으로 접속하면 이 메서드가 실행
-    // 처음 조인폼 화면 출력하는 get 방식 요청
     @GetMapping("/join")
     public String join(){
         return "join";
     }
 
-    // 해당 조인폼 화면에서 폼의 전송 이벤트 발생시 데이터 가공하는 post 방식 요청
-    // 이때 폼의 모든 요소를 일일이 전달하는 것이 아닌 DTO 파일로 감싸고 전달
-    // Model 타입의 파라미터, 서비스에 전달된 데이터를 뷰템플릿에 전달하기 위한 전용 클래스
     @PostMapping("/join/create")
-    public String create(
-            // JoinDTO 타입의 formDTO 라는 파라미터로 기존 @RequestParam 대체
-            // formDTO 에는 모든 폼 데이터가 객체로 그룹화되어 전달됨
-            @ModelAttribute JoinDTO formDTO,
-            Model model //Model 타입의 파라미터 추가
-    ){
-        // 서비스 메서드에 DTO
-        String msg = joinService.processJoin(formDTO);
-        // 추가한 파라미터로 자동 전달된 인스턴스의 addAttribute 라는 메서드를 통해
-        // ("data": 전달할 문자열) 형식의 데이터를 index 템플릿에 전달
-        model.addAttribute("data", msg);
-        List<JoinEntity> users = joinService.getAllUsers();
-        System.out.println(users);
+    public String create( JoinDTO formDTO ){
+        joinService.processJoin(formDTO);
         return "redirect:/admin";
-    /*  템플릿에 데이터 전달 흐름
-        1.컨트롤러에서 Model 타입의 파라미터 연결
-        2.서비스에서 템플릿 전달한 문자값 리턴
-        3.컨트롤러에서 전달받은 문자열을 model 의 addAttribute 메서드를 통해 index 템플릿에 전달
-    */
     }
+
     @GetMapping("/admin")
-    // Spring 이 자동 주입하는 객체로, 컨트롤러 → 뷰 템플릿으로 데이터를 전달
     public String showAdminPage(Model model){
-        // getAllUsers(): DB 에서 사용자 정보를 조회하는 메서드
         List<JoinEntity> users = joinService.getAllUsers();
-        // ${users} 또는 th:each="user : ${users}" 로 사용
         model.addAttribute("users", users);
         return "admin";
     }
+
 
     @GetMapping("/admin/del/{id}")
     public String delUser(@PathVariable Long id){
@@ -71,30 +46,19 @@ public class JoinController {
     // 수정 버튼 클릭시 해당 메서드 어노테이션 호출
     @GetMapping("/admin/edit/{id}")
     public String editUser(@PathVariable Long id, Model model){
-        // 서비스 메서드인 getUserById 에 클릭한 게시글의 아이디값 전달해서 게시글 반환받음
+        //서비스 메서드인 getUserById에 클릭한 게시글의 아이디값 전달해서 게시글 반환받음
         JoinEntity user = joinService.getUserById(id);
-        // edit 뷰화면에 해당 데이터 전달
+        //model.addAttribute 메서드에 의해서 edit 뷰화면에 해당 데이터 전달
         model.addAttribute("user", user);
         return "edit";
     }
 
-    // 수정폼 화면에서 최종 수정이 일어나는 컨트롤러
+    //수정폼 화면에서 최종 수정이 일어나는 컨트롤러
     @PostMapping("/admin/update")
-    public String updateUser(@PathVariable JoinEntity formUser){
+    public String updateUser(JoinEntity formUser){
         joinService.updateUser(formUser);
         return "redirect:/admin";
     }
+
+
 }
-/*
-[ 브라우저 요청: GET /admin ]
-         ↓
-[ 컨트롤러 showAdminPage() ]
-         ↓
-[ joinService.getAllUsers() → DB 조회 ]
-         ↓
-[ model 에 users 리스트 저장 ]
-         ↓
-[ return "admin" → admin.html 실행 ]
-         ↓
-[ 뷰 페이지 렌더링 → 사용자에게 응답 ]
- */
--- src/main/java/com/example/basic/service/JoinService.java
+++ src/main/java/com/example/basic/service/JoinService.java
@@ -12,40 +12,31 @@ import java.util.List;
 @RequiredArgsConstructor
 public class JoinService {
     private final JoinRepo joinRepo;
-    public String processJoin(JoinDTO dto) {
-        // Entity 클래스로 부터 각 항목에 맞는 데이터를 인자로 전달하여 실제 DB에 저장할 모델 인스턴스 생성
-        JoinEntity user = new JoinEntity(null, dto.getUname(), dto.getEmail(), dto.getColors());
 
-        // 위에서 생성한 user 라는 인스턴스를 joinRepo 의 save 메서드로 전달하기만 하면 DB 에 데이터 저장됨
-        // save() 리포지토리에서 JPA 구현체가 자동으로 생성 DB 저장 전용 메서드
+    public void processJoin(JoinDTO dto) {
+        JoinEntity user = new JoinEntity(null, dto.getUname(), dto.getEmail(), dto.getColors() );
         joinRepo.save(user);
-
-        // 컨트롤링를 통해 실제 템플릿에 전달된 데이터가 맵핑된 태그문자열 반환
-        // JoinDTO 타입의 폼 객체를 전달받은 데이터를 활용한 태그 문자열 생성후 리턴
-        String result = "name:" +dto.getUname()+"<br />"
-                + "email:" +dto.getEmail()+ "<br />"
-                + "my color:" +dto.getColors();
-        return result;
     }
 
+    // 모든 DB 데이터 조회해서 반환
     public List<JoinEntity> getAllUsers(){
         return joinRepo.findAll();
     }
 
-    // id 값으로 데이터삭제하는 jpa 메서드 호출
-    public void delete(long id){
+    //id 값으로 데이터삭제하는 jpa 메서드 호출
+    public void delete(Long id){
         joinRepo.deleteById(id);
     }
 
-    // id 값으로 해당 데이터만 가져오는 메서드
-    public JoinEntity getUserById(long id){
-        // 컨트롤러부터 id 값을 전달받아 해당 id 값에 매칭되는 데이터를 반환
-        // 초기에 반환되는 값이 null 인 경우 JoinEntity 타입이 아니기 때문에 에러발생
-        // 초기 에러를 피하기 위해서 orElseThrow 을 통해 예외처리
-        return joinRepo.findById(id).orElseThrow(()->new RuntimeException("해당 아이디의 유저 없음"));
+    //id 값으로 해당 데이터만 가져오는 메서드
+    public JoinEntity getUserById(Long id){
+        //컨트롤러부터 id 값을 전달받아 해당 id 값에 매칭되는 데이터를 반환
+        //초기에 반환되는 값이 null인 경우 JoinEntity 타입이 아니기 때문에 에러발생
+        //초기 에러를피하기 위해서 onElseThrow 를 통해 예외처리
+        return joinRepo.findById(id).orElseThrow(()-> new RuntimeException("해당 아이디의 유저 없음"));
     }
 
-    public void updateUser(JoinEntity user){
+    public  void updateUser(JoinEntity user){
         joinRepo.save(user);
     }
 }
@@ -55,6 +46,6 @@ public class JoinService {
     - DB 상에 id 값에 매칭되는 데이터가 없으면 save 호출시 -> insert SQL 문으로 변환
     - DB 상에 id 값에 매칭되는 데이터가 있으면 save 호출시 -> update SQL 문으로 변환
 
-    - DTO 는 아직 DB 에 저장되지 않은 상태에서 폼필드값으로 구분해서 생성한 데이터틀 (처음 DB 에 데이터 저장시, 폼 값 전달시 필요)
-    - Entity 는 DB 에 저장된 데이터의 틀을 강제하는 스키마 개념 (수정시 필요)
+    - DTO 는 아직 DB에 저장되지 않은 상태에서 폼필드값으로 구분해서 생성한 데이터틀 (처음 DB에 데이터 저장시, 폼 값 전달시 필요)
+    - Entity 는 DB에 저장된 데이터의 틀을 강제하는 스키마 개념 (수정시 필요)
  */
