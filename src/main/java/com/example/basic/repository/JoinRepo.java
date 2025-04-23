14368e041565be440f3a7223306dfed22a15b4c6
ChangMin59 <dy2ghks@gmail.com>
2025-04-23 13:57:31 +0900

    로그인 템플릿 생성

--- src/main/java/com/example/basic/repository/JoinRepo.java
+++ src/main/java/com/example/basic/repository/JoinRepo.java
@@ -3,7 +3,10 @@ package com.example.basic.repository;
 import com.example.basic.entity.JoinEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-// jPA 인터페이스만 정의
-// 이때 JpaRepository 를 상속하면서 전달되는 데이터를 JoinEntity 내용으로 강제하는 인터페이스 생성
+//Jpa 라는 인터페이스에 JoinEntity 내용을 기반으로한 JoinRepo 라는 커스텀 인터페이스 확장
 public interface JoinRepo extends JpaRepository<JoinEntity, Long> {
 }
+
+/*
+    interface : 클래스에서 어떤 메서드가 필수로 정의되야 될지 지정하는 설계서
+*/
