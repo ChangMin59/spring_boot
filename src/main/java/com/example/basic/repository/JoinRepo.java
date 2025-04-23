182de90a81eeb3c979a87269006906f8aa5cc941
ChangMin59 <dy2ghks@gmail.com>
2025-04-23 14:33:10 +0900

    로그인 세션에 담기

--- src/main/java/com/example/basic/repository/JoinRepo.java
+++ src/main/java/com/example/basic/repository/JoinRepo.java
@@ -3,12 +3,11 @@ package com.example.basic.repository;
 import com.example.basic.entity.JoinEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-//Jpa 라는 인터페이스에 JoinEntity 내용을 기반으로한 JoinRepo 라는 커스텀 인터페이스 확장
+//Jpa라는 인터페이스에 JoinEntity내용을 기반으로한 JoinRepo라는 커스텀 인터페이스 확장
 public interface JoinRepo extends JpaRepository<JoinEntity, Long> {
-    //uname 값과 email 정보를 활용해서 기존 DB 상의 데이터 유무를 찾는 커스텀 메서드 인터페이스에 등록
+    //uname값과 email정보를 활용해서 기존 DB상의 데이터 유무를 찾는 커스텀 메서드 인터페이스에 등록
     JoinEntity findByUnameAndEmail(String uname, String email);
 }
-
 /*
     interface : 클래스에서 어떤 메서드가 필수로 정의되야 될지 지정하는 설계서
 */
